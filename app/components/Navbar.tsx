63c34a0072c15f69e83c0c19a420c2c3ac8879d5
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-01-31 21:16:58 +0700

    feat(auth): add GitHub provider to NextAuth

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import { auth, signIn, signOut } from "@/app/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <>
+      <header className="px-5 py-3 bg-white shadow-sm">
+        <nav className="flex justify-between items-center">
+          <Link href="/">
+            <Image
+              src="/logo-bimbel.png"
+              alt="logo-baca-house"
+              width={100}
+              height={100}
+            />
+          </Link>
+
+          <div className="flex items-center gap-5 text-black">
+            {session && session?.user ? (
+              <>
+                <Link href="/startup/create">
+                  <span>Create</span>
+                </Link>
+
+                <form
+                  action={async () => {
+                    "use server";
+                    await signOut({ redirectTo: "/" });
+                  }}
+                >
+                  <button type="submit">Log Out</button>
+                </form>
+
+                <Link href={`/user/${session?.id}`}>
+                  <span>{session?.user?.name}</span>
+                </Link>
+              </>
+            ) : (
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn("github");
+                }}
+              >
+                <button type="submit">Log In</button>
+              </form>
+            )}
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
